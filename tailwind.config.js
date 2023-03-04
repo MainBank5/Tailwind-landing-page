5dd8943dc448d58338e327acbe53b69a12b2bf1a
Eliud Karuga <eliudkaruga97@gmail.com>
2023-03-04 02:11:21 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors:{
+        brightred: 'hsl(12, 88%, 59%)',
+        darkblue: 'hsl(228, 39%, 23%)',
+        darkgrayishblue: 'hsl(227, 12%, 61%)',
+        verydarkblue: 'hsl(233, 12%, 13%)',
+        verypalered: 'hsl(13, 100%, 96%)',
+        verylightgray: 'hsl(0, 0%, 98%)'
+      }
+    },
+  },
+  plugins: [],
+}
